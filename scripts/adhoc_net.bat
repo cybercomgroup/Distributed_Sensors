7c60c4c39fbc06aa48f0dbc29a04f80060c94ed7
Fredrik Mårlind <fmarlind@gmail.com>
2017-02-08 11:14:26 +0100

    Added some scripts
    -- One that will fetch everything from master branch -- Another that is for windows and will connect to the ad hoc network

---
+++ scripts/adhoc_net.bat
@@ -0,0 +1,12 @@
+
+@netsh wlan set profileparameter DSadhoc connectiontype=ibss connectionmode=manual
+@netsh wlan connect DSadhoc
+
+::Change "WiFi" to fit your wireless network adapter name, change ip to fit your needs
+@netsh int ip set address "WiFi" static 192.168.0.1 255.255.255.0
+
+@echo press anything to disconnect from AdHoc
+@pause
+
+@netsh wlan disconnect
+@netsh int ip set address "WiFi" dhcp
---
+++ scripts/gPull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd
+git clone https://github.com/cybercomgroup/Distributed_Sensors.git
+cd Distributed_Sensors
+git checkout master
+git pull
