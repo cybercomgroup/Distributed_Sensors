e17ba6dab585b3c8f776654727c642cf1a835848
Fredrik Mårlind <fmarlind@gmail.com>
2017-02-21 11:29:58 +0100

    Responds to every packet with hej

--- main.cpp
+++ main.cpp
@@ -36,21 +36,31 @@ int main(int argc, char** argv) {
 		boost::lexical_cast<int>(argv[2]));
 		std::cout << "Local bind " << local_endpoint << std::endl;
 		
+			
+		//boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::udp::v4(), 12345);
 		
-		boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::udp::v4(), 12345);
+		//Recieve socket
 		boost::asio::ip::udp::socket socket(io_service);
 		socket.open(boost::asio::ip::udp::v4());
-		socket.bind(local_endpoint);
+		socket.bind(local_endpoint); 
 	
+		//Send socket
+		boost::asio::ip::udp::socket send_socket(io_service,boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(),0));
+		
 	
 		boost::array<char, 128> recv_buf;
-    
-		boost::asio::ip::udp::endpoint sender_endpoint;
-		size_t len = socket.receive_from(
-        boost::asio::buffer(recv_buf), sender_endpoint);
+		while(1) {
+			boost::asio::ip::udp::endpoint sender_endpoint;
+			size_t len = socket.receive_from(
+			boost::asio::buffer(recv_buf), sender_endpoint);
 
-		std::cout << "Recieved data from IPv4:" << sender_endpoint.address().to_string() << std::endl;
-		std::cout.write(recv_buf.data(), len);	
+			std::cout << "Recieved data from IPv4:" << sender_endpoint.address().to_string() << std::endl;
+			std::cout.write(recv_buf.data(), len);	
+			//send
+			
+			 
+			send_socket.send_to(boost::asio::buffer("hej",4),sender_endpoint);
+		}
 	
 	}
 	catch (std::exception& e){
