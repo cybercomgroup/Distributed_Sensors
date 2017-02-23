a93e9e79d2322d5cabce6196a44d0b55e92be895
Tobias Laving <tobbeman1994@gmail.com>
2017-02-23 22:12:16 +0100

    Hello and response now work, atleast in controlled enviorment with computer

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/ref.hpp>
 #include <string>
 
 #include "client.h"
@@ -21,10 +22,10 @@ using namespace std;
 
 bool sendHellos(boost::asio::io_service &io_service, boost::asio::ip::udp::endpoint &local_endpoint){
 	
-	string validIps[] = {"192.168.0.11","192.168.0.17","192.168.0.1"};
+	string validIps[] = {"192.168.0.11","192.168.0.17","192.168.0.1", "169.254.191.147"};
 		for( int i = 0; i < sizeof(validIps)/sizeof(validIps[0]); i++){
 			boost::asio::ip::udp::socket send_socket(io_service,boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(),0));
-			boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::address::from_string(validIps[i]), 51885);
+			boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::address::from_string(validIps[i]), 58292);
 			send_socket.send_to(boost::asio::buffer("H",1),endpoint);
 		}
 }
@@ -45,11 +46,12 @@ void listenForHellos(boost::asio::io_service &io_service, boost::asio::ip::udp::
 		size_t len = socket.receive_from(
 		boost::asio::buffer(recv_buf), sender_endpoint);
 
-		std::cout << "Recieved data from IPv4:" << sender_endpoint.address().to_string() << std::endl;
+		std::cout << "Recieved data from IPv4: " << sender_endpoint.address().to_string() << std::endl;
 		std::cout.write(recv_buf.data(), len);	
 				
-			//Respong to message
-		if(recv_buf.data() == "H"){
+		//Respong to message
+		if(strcmp(recv_buf.data(), "H") == 0){
+			boost::asio::ip::udp::socket send_socket(io_service,boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(),0));
 			send_socket.send_to(boost::asio::buffer(sensor,sensor.size()),sender_endpoint);	
 		}
 	}
@@ -74,11 +76,14 @@ int main(int argc, char** argv) {
 		boost::lexical_cast<int>(argv[2]));
 		std::cout << "Local bind " << local_endpoint << std::endl;
 		
-		
+		//
 		sendHellos(boost::ref(io_service), boost::ref(local_endpoint));
 		listenForHellos(boost::ref(io_service), boost::ref(local_endpoint));
 		
-		/*
+		
+		
+		
+		
 		//boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::udp::v4(), 12345);
 		
 		//Recieve socket
@@ -90,8 +95,8 @@ int main(int argc, char** argv) {
 		
 		boost::asio::ip::udp::socket send_socket(io_service,boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(),0));
 
-		boost::asio::ip::udp::endpoint sender_endpoint(boost::asio::ip::address::from_string("192.168.0.1"), 51885);
-		send_socket.send_to(boost::asio::buffer("hej",4),sender_endpoint);
+		//boost::asio::ip::udp::endpoint sender_endpoint(boost::asio::ip::address::from_string("192.168.0.1"), 51885);
+		//send_socket.send_to(boost::asio::buffer("hej",4),sender_endpoint);
 		
 		boost::array<char, 128> recv_buf;
 		
@@ -107,7 +112,7 @@ int main(int argc, char** argv) {
 			//send
 			send_socket.send_to(boost::asio::buffer("hej",4),sender_endpoint);
 			
-		}*/
+		}
 	
 	}catch (std::exception& e){
 		std::cerr << e.what() << std::endl;
--- server.cpp
+++ server.cpp
@@ -10,34 +10,6 @@
 using namespace std;
 
  
-void server(){listenForNode();}
- 
- 
-void listenForNode(){
-	string sensor = "temp";
-	boost::array<char, 128> recv_buf;
-	while(1) {
-			
-		boost::asio::ip::udp::endpoint sender_endpoint;
-		size_t len = socket.receive_from(
-		boost::asio::buffer(recv_buf), sender_endpoint);
-
-		std::cout << "Recieved data from IPv4:" << sender_endpoint.address().to_string() << std::endl;
-		std::cout.write(recv_buf.data(), len);	
-			
-		
-		
-		//send
-		send_socket.send_to(boost::asio::buffer(sensor,sensor.size()),sender_endpoint);	
-		}
-}
-
-void sendHellos(){
-	
-		string validIps[] = {"192.168.0.11","192.168.0.17","192.168.0.1"};
-		for( int i = 0; i < sizeof(validIps)/sizeof(validIps[0]); i++){
-			boost::asio::ip::udp::socket send_socket(io_service,boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(),0));
-			boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::address::from_string(validIps[i]), 51885);
-			send_socket.send_to(boost::asio::buffer("Hello",5),endpoint);
-		}
+void server(){
+	std::cout << "SREVER HERE" << std::endl;
 }
