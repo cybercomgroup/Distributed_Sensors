aaa21f815abeea378063c1b4e8cec13cbfb8bee3
Tobias Laving <tobbeman1994@gmail.com>
2017-02-23 22:13:16 +0100

    Modifed and laid groundwork for improvment to transfer

--- scripts/transfer.bat
+++ scripts/transfer.bat
@@ -1,12 +1,23 @@
-C:\ProgramData\chocolatey\bin\pscp.exe -scp -pw distsensor C:\Chalmers\Distributed_Sensors\main.cpp pi@192.168.0.11:/home/pi/Distributed_Sensors/main.cpp
+set pspath = C:\Program Files\PuTTY
+set dipath = C:\Tobbe\Skola\Programming\DAT065\Distributed_Sensors
+set piip = 169.254.11.199
+::169.254.11.199 = 17 på lina
+:: fredriks dipath: C:\Chalmers\Distributed_Sensors\
+:: fredriks pspath: C:\ProgramData\chocolatey\bin\pscp.exe
 
-C:\ProgramData\chocolatey\bin\pscp.exe -scp -pw distsensor C:\Chalmers\Distributed_Sensors\client.cpp pi@192.168.0.11:/home/pi/Distributed_Sensors/client.cpp
+C:\"Program Files"\PuTTY\pscp.exe -scp -pw distsensor C:\Tobbe\Skola\Programming\DAT065\Distributed_Sensors\main.cpp pi@169.254.11.199:/home/pi/Distributed_Sensors/main.cpp
 
-C:\ProgramData\chocolatey\bin\pscp.exe -scp -pw distsensor C:\Chalmers\Distributed_Sensors\client.h pi@192.168.0.11:/home/pi/Distributed_Sensors/client.h
+C:\"Program Files"\PuTTY\pscp.exe -scp -pw distsensor C:\Tobbe\Skola\Programming\DAT065\Distributed_Sensors\client.cpp pi@169.254.11.199:/home/pi/Distributed_Sensors/client.cpp
 
-C:\ProgramData\chocolatey\bin\pscp.exe -scp -pw distsensor C:\Chalmers\Distributed_Sensors\Makefile pi@192.168.0.11:/home/pi/Distributed_Sensors/Makefile
+C:\"Program Files"\PuTTY\pscp.exe -scp -pw distsensor C:\Tobbe\Skola\Programming\DAT065\Distributed_Sensors\client.h pi@169.254.11.199:/home/pi/Distributed_Sensors/client.h
+
+C:\"Program Files"\PuTTY\pscp.exe -scp -pw distsensor C:\Tobbe\Skola\Programming\DAT065\Distributed_Sensors\server.cpp pi@169.254.11.199:/home/pi/Distributed_Sensors/server.cpp
+
+C:\"Program Files"\PuTTY\pscp.exe -scp -pw distsensor C:\Tobbe\Skola\Programming\DAT065\Distributed_Sensors\server.h pi@169.254.11.199:/home/pi/Distributed_Sensors/server.h
+
+C:\"Program Files"\PuTTY\pscp.exe -scp -pw distsensor C:\Tobbe\Skola\Programming\DAT065\Distributed_Sensors\Makefile pi@169.254.11.199:/home/pi/Distributed_Sensors/Makefile
 
 
-plink -v 192.168.0.11 -l pi -pw distsensor -batch -m commands.txt
+plink -v 169.254.11.199 -l pi -pw distsensor -batch -m commands.txt
 pause
 transfer.bat
