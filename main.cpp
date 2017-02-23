27be5358cf3d826c8e42e77bf0ded01ec92376f8
Tobias Laving <tobbeman1994@gmail.com>
2017-02-23 16:39:07 +0100

    Ready to test hello

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@
 using namespace std;
 
 
-bool sendHellos(){
+bool sendHellos(boost::asio::io_service &io_service, boost::asio::ip::udp::endpoint &local_endpoint){
 	
 	string validIps[] = {"192.168.0.11","192.168.0.17","192.168.0.1"};
 		for( int i = 0; i < sizeof(validIps)/sizeof(validIps[0]); i++){
@@ -28,39 +28,30 @@ bool sendHellos(){
 			send_socket.send_to(boost::asio::buffer("H",1),endpoint);
 		}
 }
-void listenForNode(boost::asio::io_service io_service){
+void listenForHellos(boost::asio::io_service &io_service, boost::asio::ip::udp::endpoint &local_endpoint){
 	
 	//Set up
-	try{
-		boost::asio::io_service io_service;	
-		boost::asio::ip::udp::endpoint local_endpoint(
-		boost::asio::ip::address::from_string(argv[1]), 
-		boost::lexical_cast<int>(argv[2]));
-		std::cout << "Local bind " << local_endpoint << std::endl;
 		
-		boost::asio::ip::udp::socket socket(io_service);
-		socket.open(boost::asio::ip::udp::v4());
-		socket.bind(local_endpoint); 
+	boost::asio::ip::udp::socket socket(io_service);
+	socket.open(boost::asio::ip::udp::v4());
+	socket.bind(local_endpoint); 
 		
 		
-		string sensor = "temp";
-		boost::array<char, 128> recv_buf;
-		while(1) {
+	string sensor = "temp";
+	boost::array<char, 128> recv_buf;
+	while(1) {
 				
-			boost::asio::ip::udp::endpoint sender_endpoint;
-			size_t len = socket.receive_from(
-			boost::asio::buffer(recv_buf), sender_endpoint);
+		boost::asio::ip::udp::endpoint sender_endpoint;
+		size_t len = socket.receive_from(
+		boost::asio::buffer(recv_buf), sender_endpoint);
 
-			std::cout << "Recieved data from IPv4:" << sender_endpoint.address().to_string() << std::endl;
-			std::cout.write(recv_buf.data(), len);	
+		std::cout << "Recieved data from IPv4:" << sender_endpoint.address().to_string() << std::endl;
+		std::cout.write(recv_buf.data(), len);	
 				
 			//Respong to message
-			if(recv_buf.data() == "H"){
-				send_socket.send_to(boost::asio::buffer(sensor,sensor.size()),sender_endpoint);	
-			}
+		if(recv_buf.data() == "H"){
+			send_socket.send_to(boost::asio::buffer(sensor,sensor.size()),sender_endpoint);	
 		}
-	}catch (std::exception& e){
-		std::cerr << e.what() << std::endl;
 	}
 }
 
@@ -77,17 +68,17 @@ int main(int argc, char** argv) {
 		return 1;
 		}
 		
-		
-		
-		
 		boost::asio::io_service io_service;
-		
 		boost::asio::ip::udp::endpoint local_endpoint(
 		boost::asio::ip::address::from_string(argv[1]), 
 		boost::lexical_cast<int>(argv[2]));
 		std::cout << "Local bind " << local_endpoint << std::endl;
 		
-		/*	
+		
+		sendHellos(boost::ref(io_service), boost::ref(local_endpoint));
+		listenForHellos(boost::ref(io_service), boost::ref(local_endpoint));
+		
+		/*
 		//boost::asio::ip::udp::endpoint endpoint(boost::asio::ip::udp::v4(), 12345);
 		
 		//Recieve socket
